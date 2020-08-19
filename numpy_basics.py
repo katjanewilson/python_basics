62353dc6006ec163f77a9ce18d79fe6a548e2278
Kat Wilson <katjanewilson@gmail.com>
2020-08-19 14:29:12 +0000

    edits

---
+++ numpy_basics.py
@@ -0,0 +1,21 @@
+#create 1D numpy array from a list
+array_1d = np.array([1, 2, 3, 4, 5])
+
+print (array_1d)
+
+list_1 = [11, 22, 33, 44, 55]
+array_l1 = np.array(list_1)
+
+print(array_l1)
+
+print ("NumPy version - " + np.version.version)
+print(array_1d.shape)
+type(array_1d)
+plain_range = range(4,12)
+list(plain_range)
+interval_range = range(1, 15, 2)
+list(interval_range)
+list(range(15, 1, -2))
+
+
+## add two d arrays, etc...
